1cdd19f0b8f3852be070755d5bd8a698aa0fb2a2
wshuai <wangshuai@reapal.com>
2018-09-13 10:52:16 +0800

    111

--- src/main.js
+++ src/main.js
@@ -122,6 +122,38 @@ Axios.interceptors.response.use(res => {
 Vue.prototype.$http = Axios
 Vue.http = Axios
 
+Vue.prototype.getNowFormatDate = function() {
+    var date = new Date();
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = year + seperator1 + month + seperator1 + strDate + " 00:00:00";
+    return currentdate;
+};
+
+Vue.prototype.getWeekBeforeFormatDate = function() {
+    var date = new Date(new Date() - 7 * 24 * 3600 * 1000);
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = year + seperator1 + month + seperator1 + strDate + " 00:00:00";
+    return currentdate;
+};
+
 
 Vue.config.productionTip = false
 
--- src/manage/order.vue
+++ src/manage/order.vue
@@ -22,7 +22,7 @@
 
       <el-row>
         <el-col :span="12">交易日期
-          <el-date-picker v-model="tradetimes" type="daterange" value-format="yyyy-MM-dd HH:mm:ss" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期">
+          <el-date-picker v-model="tradetimes" type="daterange" format="yyyy-MM-dd" value-format="yyyy-MM-dd HH:mm:ss" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期">
           </el-date-picker>
         </el-col>
 
@@ -166,6 +166,7 @@ export default {
     this.getLoginUser();
   },
   mounted() {
+    this.tradetimes = [this.getWeekBeforeFormatDate(), this.getNowFormatDate()];
     this.onQueryClick(1);
   }
 };
